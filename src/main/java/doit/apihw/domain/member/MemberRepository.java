3322c757e60e4c0734d3f469ff5d63bd2050f7c0
john0609 <nihaojohn@naver.com>
2024-10-08 19:50:07 +0900

    민경준 2주차 과제

--- src/main/java/doit/apihw/domain/member/MemberRepository.java
+++ src/main/java/doit/apihw/domain/member/MemberRepository.java
@@ -1,11 +1,19 @@
 package doit.apihw.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByMemberLoginId(String memberLoginId);
     // TODO : 회원 이름으로 회원 정보를 조회한다.
+    List<Member> findMemberByMemberName(String memberName);
 
     // TODO : 회원 로그인 ID와 비밀번호로 회원 정보를 조회한다.
+    Member findMemberByMemberLoginIdAndMemberPassword(String memberName, String memberPassword);
+
+    List<Member> findAllByOrderByMemberLoginIdDesc();
 
 }
