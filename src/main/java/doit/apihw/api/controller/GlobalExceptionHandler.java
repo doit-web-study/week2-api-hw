31f95ccfd3fc8adc2d5d1d819a878b74bc89fd81
sangjun <sangjun1389@ajou.ac.kr>
2024-10-09 05:44:29 +0900

    Change GlobalException Handler for more logging

--- src/main/java/doit/apihw/api/controller/GlobalExceptionHandler.java
+++ src/main/java/doit/apihw/api/controller/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package doit.apihw.api.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -17,6 +19,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleException(Exception e) {
+        log.error("처리되지 않은 예외입니다.", e);
         return "예상치 못한 오류가 발생했습니다.";
     }
 }
