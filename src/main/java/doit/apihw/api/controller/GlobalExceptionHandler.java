a2d0368255d8a9e04468a421f9e7f5477dcedc50
sangjun <sangjun1389@ajou.ac.kr>
2024-10-08 21:12:34 +0900

    Add GlobalException Handler

---
+++ src/main/java/doit/apihw/api/controller/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package doit.apihw.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleIllegalArgumentException(IllegalArgumentException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception e) {
+        return "예상치 못한 오류가 발생했습니다.";
+    }
+}
